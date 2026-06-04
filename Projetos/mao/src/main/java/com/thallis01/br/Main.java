ca88c89a4eea77354c0080c9c8e8c0be2af789c2
Thallis Vitor <thallisvitor0@gmail.com>
2026-06-04 19:22:52 -0300

    Add funcionalidade de TV em mao

--- Projetos/mao/src/main/java/com/thallis01/br/Main.java
+++ Projetos/mao/src/main/java/com/thallis01/br/Main.java
@@ -13,7 +13,8 @@ import javax.swing.JFrame;
 
 public class Main {
     private static Process processoNavegador = null;
-    private static boolean videoAberto = false; 
+    private static boolean navegadorAbertoIA = false; 
+    private static boolean maoJaDetectada = false;
     public static void main(String[] args) {
         OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(0);
         try {
@@ -21,50 +22,54 @@ public class Main {
             CanvasFrame janela = new CanvasFrame("Mão", CanvasFrame.getDefaultGamma() / grabber.getGamma());
             janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             OpenCVFrameConverter.ToMat converter = new OpenCVFrameConverter.ToMat();
-            String urlVideo = "https://github.com/THALLIS01-git";
+            String urlIA = "https://gemini.google.com/app";           
             Mat limiteInferior = new Mat(new Scalar(0, 30, 60, 0));
             Mat limiteSuperior = new Mat(new Scalar(20, 150, 255, 0));
-            System.out.println("Ligado");
+            System.out.println("Ligado");            
             while (janela.isVisible()) {
                 Frame frame = grabber.grab();
                 if (frame == null) break;
-                Mat imagemMat = converter.convert(frame);
+                Mat imagemMat = converter.convert(frame);               
                 Rect zonaAtivacao = new Rect(50, 50, 200, 200);
                 Mat regiaoAnalise = new Mat(imagemMat, zonaAtivacao);
                 Mat imagemHSV = new Mat();
-                opencv_imgproc.cvtColor(regiaoAnalise, imagemHSV, opencv_imgproc.COLOR_BGR2HSV);
+                opencv_imgproc.cvtColor(regiaoAnalise, imagemHSV, opencv_imgproc.COLOR_BGR2HSV);               
                 Mat mascaraPele = new Mat();
                 opencv_core.inRange(imagemHSV, limiteInferior, limiteSuperior, mascaraPele);
-                int pixelsPele = opencv_core.countNonZero(mascaraPele);
+                int pixelsPele = opencv_core.countNonZero(mascaraPele);                
                 imagemHSV.close();
                 mascaraPele.close();
                 regiaoAnalise.close();
                 if (pixelsPele > 8000) {
-                opencv_imgproc.rectangle(imagemMat, zonaAtivacao, new Scalar(0, 255, 0, 0), 3, 0, 0);
-                opencv_imgproc.putText(imagemMat, "MAO DETECTADA", new org.bytedeco.opencv.opencv_core.Point(50, 40), 
-                opencv_imgproc.FONT_HERSHEY_SIMPLEX, 0.7, new Scalar(0, 255, 0, 0), 2, 0, false);
-                if (!videoAberto) {
-                System.out.println("Mão detectada");
-                videoAberto = true; 
-                String comando = "cmd /c start chrome \"" + urlVideo + "\"";
-                processoNavegador = Runtime.getRuntime().exec(comando);
-                }    
+                    opencv_imgproc.rectangle(imagemMat, zonaAtivacao, new Scalar(0, 255, 0, 0), 3, 0, 0);
+                    opencv_imgproc.putText(imagemMat, "MAO DETECTADA", new org.bytedeco.opencv.opencv_core.Point(50, 40), 
+                            opencv_imgproc.FONT_HERSHEY_SIMPLEX, 0.7, new Scalar(0, 255, 0, 0), 2, 0, false);
+                    if (!maoJaDetectada) {
+                        maoJaDetectada = true;                       
+                        if (!navegadorAbertoIA) {
+                            System.out.println("Mão detectada: Abrindo Navegador,ligando a TV...");
+                            navegadorAbertoIA = true; 
+                            String comando = "cmd /c start chrome \"" + urlIA + "\"";
+                            processoNavegador = Runtime.getRuntime().exec(comando);
+                            Tv.ligarTV();
+                        } else {
+                            System.out.println("Mão detectada: Fechando Navegador...");
+                            navegadorAbertoIA = false;
+                            Runtime.getRuntime().exec("taskkill /F /IM chrome.exe");
+                            processoNavegador = null;    
+                        }
+                    }                   
                 } else {
                     opencv_imgproc.rectangle(imagemMat, zonaAtivacao, new Scalar(0, 0, 255, 0), 2, 0, 0);
-                    opencv_imgproc.putText(imagemMat, "✋", new org.bytedeco.opencv.opencv_core.Point(50, 40), 
-                    opencv_imgproc.FONT_HERSHEY_SIMPLEX, 0.7, new Scalar(0, 0, 255, 0), 2, 0, false);
-                    if (videoAberto) {
-                    System.out.println("Mão removida Fechando");
-                    videoAberto = false;
-                    Runtime.getRuntime().exec("taskkill /F /IM chrome.exe");
-                    processoNavegador = null;    
-                    }
+                    opencv_imgproc.putText(imagemMat, "Aguardando...", new org.bytedeco.opencv.opencv_core.Point(50, 40), 
+                            opencv_imgproc.FONT_HERSHEY_SIMPLEX, 0.7, new Scalar(0, 0, 255, 0), 2, 0, false);
+                    maoJaDetectada = false;
                 }
                 janela.showImage(converter.convert(imagemMat));
                 Thread.sleep(50);
-            }
+            }          
             limiteInferior.close();
-            limiteSuperior.close();
+            limiteSuperior.close();           
             if (processoNavegador != null && processoNavegador.isAlive()) {
                 Runtime.getRuntime().exec("taskkill /F /IM chrome.exe");
             }
---
+++ Projetos/mao/src/main/java/com/thallis01/br/Tv.java
@@ -0,0 +1,46 @@
+package com.thallis01.br;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class Tv {
+    public static void ligarTV() {
+        String macDaTV = "XXXXXXXXXXXXXXXX"; //  MAC TV
+        String ipBroadcast = "XXXXXXXXXXXXXXXX"; // IP da rede
+        int porta = 9; // Porta(Wake-on-LAN)
+        try {
+            byte[] macBytes = converterMacParaBytes(macDaTV);
+            byte[] pacoteBytes = new byte[6 + 16 * macBytes.length];
+            for (int i = 0; i < 6; i++) {
+                pacoteBytes[i] = (byte) 0xff;
+            }
+            for (int i = 6; i < pacoteBytes.length; i += macBytes.length) {
+                System.arraycopy(macBytes, 0, pacoteBytes, i, macBytes.length);
+            }
+            InetAddress endereço = InetAddress.getByName(ipBroadcast);
+            DatagramPacket datagrama = new DatagramPacket(pacoteBytes, pacoteBytes.length, endereço, porta);
+            DatagramSocket socket = new DatagramSocket();
+            socket.send(datagrama);
+            socket.close();
+            System.out.println("Ligar a TV");
+        } catch (Exception e) {
+            System.out.println("Erro ao tentar ligar a TV: " + e.getMessage());
+        }
+    }
+    private static byte[] converterMacParaBytes(String macStr) throws IllegalArgumentException {
+        byte[] bytes = new byte[6];
+        String[] hex = macStr.split("(\\:|\\-)");
+        if (hex.length != 6) {
+            throw new IllegalArgumentException("MAC inválido. formato correto: AA:BB:CC:DD:EE:FF");
+        }
+        try {
+            for (int i = 0; i < 6; i++) {
+                bytes[i] = (byte) Integer.parseInt(hex[i], 16);
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Caracteres inválidos no MAC.");
+        }
+        return bytes;
+    }
+}
